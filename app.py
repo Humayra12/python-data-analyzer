c0c91458a4accf44e91eab24b3a4b8d383f4b826
Humayra Islam <ihumayra12@gmail.com>
2026-01-13 11:26:24 -0500

    Lecture 7 Commit 3: Add basics template to display weather data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from analyzer import analyze_numbers
 
 app = Flask(__name__)
@@ -18,6 +18,16 @@ def analyze():
         return render_template('results.html', results=result, numbers=numbers)
     except ValueError:
         return render_template('index.html', error="Invalid input. Please enter valid numbers.")
+
+from external_api import fetch_hourly_temperature
+@app.route("/test-weather")
+def test_weather():
+    lantitude = 40.7128
+    longitude = -74.0060
+    times, temperatures = fetch_hourly_temperature(lantitude, longitude)
+    return render_template('weather_analysis_results.html', weather_data=[
+        {"time": t, "temperature": temp} for t, temp in zip(times, temperatures)
+    ])
 
 if __name__ == '__main__':
     app.run(debug=True) 
